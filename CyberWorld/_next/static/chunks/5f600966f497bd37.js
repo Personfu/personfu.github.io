0e97574f840df025b5952e23977ca5272ab2806c
Personfu <Personfu@users.noreply.github.com>
2026-05-10 11:46:51 -0700

    Fix asset paths: add basePath to Next.js config

--- CyberWorld/_next/static/chunks/5f600966f497bd37.js
+++
@@ -1,1 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,21730,c=>{c.v(t=>Promise.all(["static/chunks/906dc0ec86c8f399.js"].map(t=>c.l(t))).then(()=>t(33901)))}]);
